4ff7eefcb459fb36746036c279159062f686067d
lancenonce <lancendavis47@gmail.com>
2024-11-05 13:01:11 +0700

    init main args

--- bir/src/prove.rs
+++ bir/src/prove.rs
@@ -1,3 +1,18 @@
 // In this module, we generate a proof of the TLS request and generate a proof of the TLS response.
 
 use std::env;
+
+use tlsn_examples::ExampleType;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[clap(default_value_t, value_enum)]
+    example_type: ExampleType,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+}
